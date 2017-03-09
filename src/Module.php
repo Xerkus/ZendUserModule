5258a159f34d9f0b5794967fa03a63dfe46a24a1
Kenneth Kataiwa <kenkataiwa@outlook.com>
2017-03-09 12:29:45 +0300

    Add zend-db dependency, remove use of autoload

--- src/Module.php
+++ src/Module.php
@@ -1,32 +1,17 @@
 <?php
 namespace Zend\UserModule;
 
-use Zend\ModuleManager\Feature\AutoloaderProviderInterface;
 use Zend\ModuleManager\Feature\ConfigProviderInterface;
 use Zend\ModuleManager\Feature\ServiceProviderInterface;
 
 class Module implements
-    AutoloaderProviderInterface,
     ConfigProviderInterface,
     ServiceProviderInterface
 {
-    public function getAutoloaderConfig()
-    {
-        return array(
-            'Zend\Loader\ClassMapAutoloader' => array(
-                __DIR__ . '/autoload_classmap.php',
-            ),
-            'Zend\Loader\StandardAutoloader' => array(
-                'namespaces' => array(
-                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
-                ),
-            ),
-        );
-    }
 
     public function getConfig($env = null)
     {
-        return include __DIR__ . '/config/module.config.php';
+        return include __DIR__ . '/../config/module.config.php';
     }
 
     public function getControllerPluginConfig()
